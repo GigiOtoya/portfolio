418446f33d85e4df033cab07f65ae4245aa7ab48
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-05-24 04:11:32 -0400

    contact form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,5 +1,28 @@
 import SectionLayout from "./sectionLayout";
 
 export default function Contact() {
-  return <SectionLayout id="contact" title="Contact"></SectionLayout>;
+  return (
+    <SectionLayout id="contact" title="Contact">
+      <form className="flex flex-col gap-y-4  max-w-xl se m-auto">
+        <div className="flex flex-col gap-1">
+          <label>
+            Name <span className="text-red-500">*</span>
+          </label>
+          <input
+            className="bg-inherit border-2 border-slate-700 rounded-sm p-2"
+            placeholder="Your name..."
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label>
+            Subject <span className="text-red-500">*</span>
+          </label>
+          <textarea
+            className="bg-inherit border-2 border-slate-700 rounded-sm p-2 min-h-72 "
+            placeholder="Your message..."
+          />
+        </div>
+      </form>
+    </SectionLayout>
+  );
 }
