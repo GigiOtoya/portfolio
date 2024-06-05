cf8f0e48cf7cb6d79edcf27e44c20819ab7b4daf
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-06-05 00:45:03 -0400

    project descriptions

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="flex max-w-5xl px-8 py-2 m-auto sticky top-0 bg-black sm:px-12">
-      <div className="flex w-full justify-between font-bold">
-        <span className="">LGO</span>
+      <div className="flex w-full justify-between font-bold py-1 rounded-md">
+        <span className="">
+          <Link href={"#home"}>LGO</Link>
+        </span>
         <span className="flex ml-auto gap-6 sm:gap-12">
           <span>
             <Link href={"#about"}>About</Link>
