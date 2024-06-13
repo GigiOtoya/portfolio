9e91d264e26b5283c7b9cd4114b26d6c6d5083a0
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-06-13 01:37:16 -0400

    intro padding left

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -4,9 +4,9 @@ export default function Intro() {
   const intro = getAboutData().intro;
   return (
     <div className="w-full">
-      <div className="w-2/4 float-end">
+      <div className="pl-4 sm:w-2/4 float-end">
         <h1 className="text-5xl text-sky-400 font-bold">Luigi Otoya</h1>
-        <p className="py-4">{intro}</p>
+        <p className="py-8 tracking-wider leading-loose">{intro}</p>
       </div>
     </div>
   );
