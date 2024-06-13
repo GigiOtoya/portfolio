54974803a32a6ff1fbb94bdea261578eaef0c113
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-06-13 04:59:51 -0400

    name contract animation

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -4,9 +4,11 @@ export default function Intro() {
   const intro = getAboutData().intro;
   return (
     <div className="w-full">
-      <div className="sm:pl-2 sm:w-2/4 float-end">
-        <h1 className="text-5xl text-sky-400 font-bold">Luigi Otoya</h1>
-        <p className="py-8 tracking-wider leading-loose">{intro}</p>
+      <div className="flex flex-col sm:pl-2 sm:w-2/4 float-end">
+        <h1 className="overflow-hidden min-h-14 text-5xl text-sky-400 font-bold animate-tracking-in-contract whitespace-nowrap">
+          Luigi Otoya
+        </h1>
+        <p className="tracking-wider leading-loose">{intro}</p>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,25 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        "tracking-in-contract":
+          "tracking-in-contract 1.0s cubic-bezier(0.215, 0.610, 0.355, 1.000)",
+      },
+      keyframes: {
+        "tracking-in-contract": {
+          "0%": {
+            "letter-spacing": "1em",
+            opacity: "0",
+          },
+          "40%": {
+            opacity: ".6",
+          },
+          to: {
+            "letter-spacing": "normal",
+            opacity: "1",
+          },
+        },
+      },
     },
     screens: {
       sm: "640px",
