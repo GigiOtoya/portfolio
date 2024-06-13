2f5390fba02d62c27553f522650015fbe84d3d7c
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-06-13 19:06:16 -0400

    page icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Luigi Otoya",
   description: "Personal website created with next.js",
+  icons: "icons/icon.svg",
 };
 
 export default function RootLayout({
