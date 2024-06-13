038a77ab38f437782bd1f27b9cb6f4062c534672
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-06-13 14:04:56 -0400

    title jitter

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   const intro = getAboutData().intro;
   return (
     <div className="w-full">
-      <div className="flex flex-col sm:pl-2 sm:w-2/4 float-end">
+      <div className=" w-full sm:pl-2 sm:w-2/4 float-end">
         <h1 className="overflow-hidden min-h-14 text-5xl text-sky-400 font-bold animate-tracking-in-contract whitespace-nowrap">
           Luigi Otoya
         </h1>
